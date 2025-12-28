c540c141deba07c5430fdeb55afbd2c57af50cb2
Mouri69 <trident_mazen@outlook.com>
2025-12-28 17:06:27 +0200

    fixed some errors

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:provider/provider.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'l10n/app_localizations.dart';
 import 'theme/app_theme.dart';
 import 'providers/prayer_provider.dart';
 import 'services/storage_service.dart';
